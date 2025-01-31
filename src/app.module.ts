b27cc942f83e68be9d84560229b52737c7d194e6
prince-konwea <konweaprince09@gmail.com>
2025-01-31 05:00:47 -0800

    feat: add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './modules/user/user.module';
-import { TransactionService } from './modules/transaction/transaction.service';
-import { TransactionController } from './modules/transaction/transaction.controller';
 import { TransactionModule } from './modules/transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/muskdb'), 
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI), 
     UserModule, TransactionModule,
   ],
   providers: [],
