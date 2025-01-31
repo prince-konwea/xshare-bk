8d29cf1a5c9fb7b2d5cf325600bac82ff051c360
prince-konwea <konweaprince09@gmail.com>
2025-01-31 05:54:07 -0800

    feat: add wild card

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
